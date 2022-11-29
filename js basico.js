3f5f5e3fddf5b7eb204553c33772e912a1b7f45c
TongasHub <ferreyragaston40@gmail.com>
2022-11-29 16:59:37 -0300

    ENTREGA JAVASCRIPT BASICO

---
+++ js basico.js
@@ -0,0 +1,276 @@
+// EJ 1
+
+  // este es un comentario de linea
+
+  /*
+  este es un comentario 
+  de bloque
+  */
+
+
+// EJ 2
+  var myName;
+
+
+// EJ 3
+  // Setup
+  var a;
+
+  // Only change code below this line
+  a = 7
+
+
+// EJ 4
+  // Setup
+  var a;
+  a = 7;
+  var b;
+
+  // Only change code below this line
+  b = a;
+
+
+// EJ 5
+  var a = 9;
+
+
+// EJ 6
+  var myFirstName = "Gaston"
+  var myLastName = "Ferreyra"
+
+
+// EJ 7
+  // Only change code below this line
+  var a= 5;
+  var b= 10;
+  var c="I am a";
+  // Only change code above this line
+
+  a = a + 1;
+  b = b + 5;
+  c = c + " String!";
+
+
+// EJ 8
+  // Variable declarations
+  var studlyCapVar;
+  var properCamelCase;
+  var titleCaseOver;
+
+  // Variable assignments
+  studlyCapVar = 10;
+  properCamelCase = "A String";
+  titleCaseOver = 9000;
+
+
+// EJ 9
+  let catName = "Oliver";
+  let catSound = "Meow!";
+
+
+// EJ 10
+  const FCC = "freeCodeCamp"; // Change this line
+  let fact = "is cool!"; // Change this line
+  fact = "is awesome!";
+  console.log(FCC, fact); // Change this line
+
+
+// EJ 11
+  const sum = 10 + 10;
+
+
+// EJ 12
+  const difference = 45 - 33;
+
+
+// EJ 13
+  const product = 8 * 10;
+
+
+// EJ 14
+  const quotient = 66 / 33;
+
+
+  
+// EJ 15
+  let myVar = 87;
+
+  // Only change code below this line
+  myVar= ++myVar;
+
+
+// EJ 16
+  let myVar = 11;
+
+  // Only change code below this line
+  myVar = --myVar;
+  
+// EJ 17
+  const ourDecimal = 5.7;
+
+  // Only change code below this line
+
+  let myDecimal = 1.1;
+
+
+// EJ 18
+  const product = 2.0 * 2.5;
+
+
+// EJ 19
+  const quotient = 4.4 / 2.0; // Change this line
+
+
+// EJ 20
+  const remainder = 11 % 3;
+
+
+// EJ 21
+  let a = 3;
+  let b = 17;
+  let c = 12;
+
+  // Only change code below this line
+  a += 12;
+  b += 9;
+  c += 7;
+
+// EJ 22
+  let a = 11;
+  let b = 9;
+  let c = 3;
+
+  // Only change code below this line
+  a -= 6;
+  b -= 15;
+  c -= 1;
+
+
+// EJ 23
+  let a = 5;
+  let b = 12;
+  let c = 4.6;
+
+  // Only change code below this line
+  a *= 5;
+  b *= 3;
+  c *= 10;
+
+
+// EJ 24
+  let a = 48;
+  let b = 108;
+  let c = 33;
+
+  // Only change code below this line
+  a /= 12;
+  b /= 4;
+  c /= 11;
+
+
+// EJ 25
+  const myStr = "I am a \"double quoted\" string inside \"double quotes\"."; // Change this line
+
+
+// EJ 26
+  const myStr = '<a href="http://www.example.com" target="_blank">Link</a>';
+
+
+// EJ 27
+  const myStr = "FirstLine\n\t\\SecondLine\nThirdLine"; // Change this line
+
+
+// EJ 28
+  const myStr = "This is the start. " + "This is the end."; // Change this 
+  
+
+// EJ 29
+  let myStr = "This is the first sentence.";
+
+  myStr += " This is the second sentence."
+
+
+// EJ 30
+  // Only change code below this line
+  const myName = "My name is ";
+  const myStr = "Hi! " + myName + "and I am well!";
+
+
+// EJ 31
+  // Change code below this line
+  const someAdjective = "pato";
+  let myStr = "Learning to code is ";
+
+  myStr += someAdjective
+
+
+// EJ 32
+  // Setup
+  let lastNameLength = 0;
+  const lastName = "Lovelace";
+
+  // Only change code below this line
+  lastNameLength = lastName.length;
+
+
+// EJ 33
+  // Setup
+  let firstLetterOfLastName = "";
+  const lastName = "Lovelace";
+
+  // Only change code below this line
+  firstLetterOfLastName = lastName[0]; // Change this line
+
+
+// EJ 34
+  // Setup
+  let myStr = "Jello World";
+
+  // Only change code below this line
+  myStr = "Hello World"; // Change this line
+  // Only change code above this line
+
+
+//  EJ 35
+  // Setup
+  const lastName = "Lovelace";
+
+  // Only change code below this line
+  const thirdLetterOfLastName = lastName[2]; // Change this line
+
+
+//  EJ 36
+  // Setup
+  const lastName = "Lovelace";
+
+  // Only change code below this line
+  const lastLetterOfLastName = lastName[lastName.length - 1]; // Change this line
+
+
+//  EJ 37
+  // Setup
+  const lastName = "Lovelace";
+
+  // Only change code below this line
+  const secondToLastLetterOfLastName = lastName[lastName.length - 2]; // Change this line
+
+
+//  EJ 38
+  const myNoun = "dog";
+  const myAdjective = "big";
+  const myVerb = "ran";
+  const myAdverb = "quickly";
+
+  // Only change code below this line
+  const wordBlanks = myNoun + " " + myVerb + " " + myAdjective + " " + myAdverb; // Change this line
+  // Only change code above this line
+
+
+// EJ 39
+  // Only change code below this line
+  const myArray = ["hola", 8];
+
+
+// EJ 40
+  // Only change code below this line
+  const myArray = [["a", "b", "c"], [1, 2, 3]];
